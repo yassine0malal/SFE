2524e10e5f0bf0b40350f9a38a8f3c5451054e64
yassine <159465917+yassine0malal@users.noreply.github.com>
2025-04-20 13:18:49 +0100

    first commit

---
+++ backend/config/config.php
@@ -0,0 +1,11 @@
+<?php
+// Fichier de configuration de la base de données
+define('DB_SERVER', 'localhost');     // Serveur MySQL
+define('DB_USERNAME', 'root'); // Nom d'utilisateur
+define('DB_PASSWORD', '');  // Mot de passe
+define('DB_NAME', 'web_design_development');        // Nom de la base de données
+
+// Vous pouvez ajouter d'autres constantes de configuration si nécessaire
+define('DB_PORT', '3306');            // Port MySQL (optionnel)
+define('DB_CHARSET', 'utf8mb4');      // Jeu de caractères
+?>
